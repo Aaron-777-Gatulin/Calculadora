28d343c5ce70b68392604ab968a28565ef4614b1
Aaron Albornoz <aaronalbornoz777@gmail.com>
2023-06-20 18:13:36 -0500

    nuevos cambios

---
+++ index.js
@@ -0,0 +1,12 @@
+const fn = ()=> {
+    const  miString = "soy un chanchito feliz"
+    let b = miString = "soy muy feliz"
+    console.log(b)
+}
+fn()
+
+const clickeado = () => {
+    let a = "chanchito feliz"
+
+    console.log(a)
+}
